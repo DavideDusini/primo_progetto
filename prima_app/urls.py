471b57ecde45067d47dc57546afd51784943d35e
Davide Dusini <davide.dusini@alessandrinimainardi.edu.it>
2023-10-05 11:15:49 +0200

    Initial commit

---
+++ prima_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from prima_app.views import homapage, welcome, lista, chi_siamo
+
+app_name="prima_app"
+urlpatterns=[
+    path('', homapage, name='homepage'),
+    path('welcome', welcome, name='welcome'),
+    path('lista',lista,name='lista'),
+    path('chi_siamo', chi_siamo, name='chi_siamo')
+]
