e2da50b0b003e973d384f0f138832da9543ef81b
Davide Dusini <davide.dusini@alessandrinimainardi.edu.it>
2023-11-10 09:32:05 +0100

    Passo 17

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home,articoloDetailView
+from .views import *
 
 app_name="news"
 
 urlpatterns=[
     path('home_articolo', home, name="home"),
-    path('articoli/<int:pk>',articoloDetailView, name="articolo_detail")
+    path('articoli/<int:pk>',articoloDetailView, name="articolo_detail"),
+    path('lista_articoli/<int:pk>',listaArticoli, name="lista_articoli_giornalista"),
+    path('lista_articoli/',listaArticoli, name="lista_articoli"),
 ]
