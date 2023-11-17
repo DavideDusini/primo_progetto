ad67a9f6f78661479bef799f92a613a05c932d34
Davide Dusini <davide.dusini@alessandrinimainardi.edu.it>
2023-11-17 09:44:10 +0100

    Passo 18

--- news/urls.py
+++ news/urls.py
@@ -7,5 +7,6 @@ urlpatterns=[
     path('home_articolo', home, name="home"),
     path('articoli/<int:pk>',articoloDetailView, name="articolo_detail"),
     path('lista_articoli/<int:pk>',listaArticoli, name="lista_articoli_giornalista"),
-    path('lista_articoli/',listaArticolo, name="lista_articoli"),
+    path('lista_articoli/',listaArticolo, name="lista_articolo"),
+    path('query_base',queryBase, name="query_base"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse, render, get_object_or_404
 from .models import *
+import datetime
 # Create your views here.
 def home(request):
   """  a = ""
@@ -52,7 +53,7 @@ def listaArticolo(request, pk=None):
 
 def queryBase(request):
   #1. Tutti gli articoli scritti da giornalisti di un certo cognome:
-  articoli_cognome = Articolo.objects.filter(giornalista_cognome='Rossi')
+  articoli_cognome = Articolo.objects.filter(giornalista__cognome='Valsecchi')
   #2. Totale
   numero_totale_articoli = Articolo.objects.count()
 
@@ -67,4 +68,51 @@ def queryBase(request):
   articoli_senza_visualizzazioni = Articolo.objects.filter(visualizzazioni=0)
 
   #6. articolo più visualizzato
-  articolo_piu_visualizzato = Articolo.objects.filter('-visualizzazioni').first()
+  articolo_piu_visualizzato = Articolo.objects.order_by('-visualizzazioni').first()
+
+  #7. Tutti i giornalisti nati dopo una certa data:
+  giornalisti_data = Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1990,1,1))
+
+  #8. Tutti gli articoli pubblicati in una data specifica
+  articoli_del_giorno =  Articolo.objects.filter(data=datetime.date(2023,1,1))
+
+  #9. tutti gli articoli pubblicati in un intervallo di date
+  articoli_periodo = Articolo.objects.filter(data__range=(datetime.date(2023,1,1),datetime.date(2023,12,31)))
+
+  #10. gli articoli scritti  da giornalisti nati prima del 1980
+  giornalisti_nati =  Giornalista.objects.filter(anno_di_nascita__lt=datetime.date(1980,1,1))
+  articoli_giornalisti =  Articolo.objects.filter(giornalista__in=giornalisti_nati)
+
+  #11. Il giornalista più giovane
+  giornalista_giovane =  Giornalista.objects.order_by('anno_di_nascita').first()
+
+  #12. Il giornalista più anziano
+  giornalista_anziano = Giornalista.objects.order_by('-anno_di_nascita').first()
+
+  #13. gli ultimi 5 articoli pubblicati
+  ultimi = Articolo.objects.order_by('-data')[:5]
+
+  #14. tutti gli articoli 
+  articoli_minime_visualizzazioni = Articolo.objects.filter(visualizzazioni__gte=100)
+
+  #15 Tutti gli articoli che contengono una certa parola nel titolo
+  articoli_parola = Articolo.objects.filter(titolo__contains='Leclerc')
+
+  context={
+    'articoli_cognome':articoli_cognome,
+    'numero_totale_articoli':numero_totale_articoli,
+    'numero_articoli_giornalista_3':numero_articoli_giornalista_3,
+    'articoli_ordinati':articoli_ordinati,
+    'articoli_senza_visualizzazioni':articoli_senza_visualizzazioni,
+    'articolo_piu_visualizzato':articolo_piu_visualizzato,
+    'giornalisti_data':giornalisti_data,
+    'articoli_del_giorno':articoli_del_giorno,
+    'articoli_periodo':articoli_periodo,
+    'articoli_giornalisti':articoli_giornalisti,
+    'giornalista_giovane':giornalista_giovane,
+    'giornalista_anziano':giornalista_anziano,
+    'ultimi':ultimi,
+    'articoli_minime_visualizzazioni':articoli_minime_visualizzazioni,
+    'articoli_parola':articoli_parola
+  }
+  return render(request, "query.html", context)
